93828e6f7339485ea05b37fb01d23095f50445cc
OlegV12 <olegvolobouev@gmail.com>
2020-10-15 17:23:32 +0300

    Lesson-1

--- Task_2.py
+++
@@ -1,8 +0,0 @@
-sec = int(input("введите количество секунд: "))
-
-sec_result = sec % 60
-minute = (sec // 60) % 60
-hour = (sec // 60) // 60
-
-
-print(f"{sec} секунд равно: {hour}:{minute}:{sec_result} в формате чч:мм:сс")
--- Task_3.py
+++
@@ -1,6 +0,0 @@
-n = int(input("введите число: "))
-
-s = int(n * 10 + n)
-u = int(n * 100 + s)
-
-print(f"{n} + {s} + {u} =", n + s + u)
